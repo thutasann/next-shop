62c6664f99d94cfb7a7bb809081a90641afe95b0
Thuta San <thutasann2002@gmail.com>
2023-08-15 09:08:58 +0630

    Feat: Stripe api in Admin Dashbaord

--- store/app/(routes)/cart/components/summary.tsx
+++ store/app/(routes)/cart/components/summary.tsx
@@ -2,22 +2,39 @@
 
 import { useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
-
 import Button from '@/components/ui/button'
 import Currency from '@/components/ui/currency'
 import { toast } from 'react-hot-toast'
 import { useCart } from '@/hooks/use-cart'
+import axios from 'axios'
 
 const Summary = () => {
   const searchParams = useSearchParams()
   const items = useCart(state => state.items)
   const removeAll = useCart(state => state.removeAll)
 
+  useEffect(() => {
+    if (searchParams.get('success')) {
+      toast.success('Payment completed')
+      removeAll()
+    }
+
+    if (searchParams.get('canceled')) {
+      toast.error('Something went wrong')
+    }
+  }, [searchParams, removeAll])
+
   const totalPrice = items.reduce((total, item) => {
     return total + Number(item.price)
   }, 0)
 
-  const onCheckout = async () => {}
+  const onCheckout = async () => {
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
+      productIds: items.map(item => item.id),
+    })
+
+    window.location = response.data.url
+  }
 
   return (
     <div className='mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8'>
